84b998384783eaaf3031c1a34cdda031783c846c
Paul Colomiets <paul@colomiets.name>
2015-08-25 22:44:58 +0300

    Add `status()`, `code()` and `signal()` method to `ExitStatus`

--- src/status.rs
+++ src/status.rs
@@ -1,12 +1,30 @@
 use std::fmt;
 use nix::sys::signal::SigNum;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum ExitStatus {
     /// Process exited normally with some exit code
     Exited(i8),
     /// Process was killed by a signal (bool flag is true when core is dumped)
     Signaled(SigNum, /* dore dumped */bool)
+}
+
+impl ExitStatus {
+    pub fn success(&self) -> bool {
+        self == &ExitStatus::Exited(0)
+    }
+    pub fn code(&self) -> Option<i32> {
+        match self {
+            &ExitStatus::Exited(e) => Some(e as i32),
+            &ExitStatus::Signaled(_, _) => None,
+        }
+    }
+    pub fn signal(&self) -> Option<i32> {
+        match self {
+            &ExitStatus::Exited(_) => None,
+            &ExitStatus::Signaled(sig, _) => Some(sig),
+        }
+    }
 }
 
 impl fmt::Display for ExitStatus {
